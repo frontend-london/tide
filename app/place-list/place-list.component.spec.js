f9b7dd7c5205b39c68da8b9ca715b2020b0ba6f8
Piotr Kołodziejczyk <contact@frontend.london>
2017-08-29 19:32:36 +0100

    added tests - place list

--- app/place-list/place-list.component.spec.js
+++ app/place-list/place-list.component.spec.js
@@ -8,25 +8,29 @@ describe('placeList', function() {
 
   // Test the controller
   describe('PlaceListController', function() {
-    var $httpBackend, ctrl;
-
-    beforeEach(inject(function($componentController, _$httpBackend_) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('api/address.json?key=AIzaSyC0on6d3nbJ8amjRosKkMXElJJe_RujTlg&latlng=51.5222735,-0.1118921,17')
-        .respond({
-          results: [
-            {name: 'Place 1'}, 
-            {name: 'Place 2'}
-          ]
-      });
-
-      $httpBackend.expectGET('api/places.json?key=AIzaSyC0on6d3nbJ8amjRosKkMXElJJe_RujTlg&location=51.5222735,-0.1118921,17&opennow=false&radius=1000&rankby=prominence&type=restaurant%7Cbar')
-      .respond({
+    var $httpBackend, 
+      ctrl,
+      config,
+      addressObj = {
+        results: [
+          {formatted_address: 'Place Address'}
+        ]
+      },
+      placesObj = {
         results: [
           {name: 'Place 1'}, 
           {name: 'Place 2'}
         ]
-    });
+      };
+
+    beforeEach(inject(function($componentController, _$httpBackend_, _config_) {
+      $httpBackend = _$httpBackend_;
+      config = _config_;
+      $httpBackend.expectGET('api/address.json?key=AIzaSyC0on6d3nbJ8amjRosKkMXElJJe_RujTlg&latlng=51.5222735,-0.1118921,17')
+        .respond(addressObj);
+
+      $httpBackend.expectGET('api/places.json?key=AIzaSyC0on6d3nbJ8amjRosKkMXElJJe_RujTlg&location=51.5222735,-0.1118921,17&opennow=false&radius=1000&rankby=prominence&type=restaurant%7Cbar')
+      .respond(placesObj);
 
       ctrl = $componentController('placeList');
     }));
@@ -34,30 +38,33 @@ describe('placeList', function() {
     it('should create a `places` property with 2 places fetched with `$http`', function() {
       jasmine.addCustomEqualityTester(angular.equals);
       ctrl.updateResults();
-      // debugger;
 
       expect(ctrl.address).toEqual({});
-
       expect(ctrl.places).toEqual({});
 
       $httpBackend.flush();
-      expect(ctrl.address).toEqual({
-        results: [
-          {name: 'Place 1'}, 
-          {name: 'Place 2'}
-        ]
-      });
-
-      expect(ctrl.places).toEqual({
-        results: [
-          {name: 'Place 1'}, 
-          {name: 'Place 2'}
-        ]
-      });
+      expect(ctrl.address).toEqual(addressObj);
+      expect(ctrl.places).toEqual(placesObj);
     });
 
     it('should set a default value for the `orderProp` property', function() {
       expect(ctrl.orderProp).toBe('prominence');
+    });
+
+    it('should set a default value for the `typeProp` property', function() {
+      expect(ctrl.typeProp).toBe('restaurant|bar');
+    });
+
+    it('should set a default value for the `openProp` property', function() {
+      expect(ctrl.openProp).toBe(false);
+    });
+
+    it('should set a default value for the `pageToken` property', function() {
+      expect(ctrl.pageToken).toBe(null);
+    });
+
+    it('should set a default value for the `location` property', function() {
+      expect(ctrl.location).toBe(config.defaultLocation);
     });
 
   });
